272a615ea0056c394a7cad4351cf981827359f7f
miclip <mlipscombe@outlook.com>
2025-12-31 13:33:06 -0700

    Remove thinq

--- update_custom_components.sh
+++ update_custom_components.sh
@@ -7,14 +7,6 @@ update_alexa_media () {
     popd > /dev/null
 }
 
-update_smartthinq_sensors () {
-    pushd ../ha-smartthinq-sensors > /dev/null
-      git pull
-      cp -r ./custom_components/smartthinq_sensors ./custom_components
-    popd > /dev/null
-}
-
 echo "Updating alexa_media..."
 update_alexa_media
-echo "Updating smartthinq_sensors..."
-update_smartthinq_sensors
+
